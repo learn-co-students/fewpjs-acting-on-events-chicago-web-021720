049db0d148c22e7ccac760d7a63f2a2566d9a06f
GreatDeveloper66 <ashaffe@gmail.com>
2020-03-30 15:07:45 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code here
+
+
+
+function moveDodgerRight() {
+  let dodger = document.getElementById('dodger').style;
+  dodger.left = parseInt(dodger.left) + 1 + "px";
+}
+
+function moveDodgerLeft() {
+  let dodger = document.getElementById('dodger').style;
+  dodger.left = parseInt(dodger.left) - 1 + "px";
+}
