7f1e9585f12a4ab36ce23d1e077dd7b6ccbd0829
jequezinho <jroverby92@gmail.com>
2020-04-30 23:12:42 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Your code here
+let dodger = document.getElementById('dodger')
+dodger.style.backgroundColor = '#FED123'
+dodger.style.bottom = "100px"
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft()
+    }
+    else if (e.key === "ArrowRight") {
+        moveDodgerRight()
+    }
+    else if (e.key === "ArrowDown") {
+        moveDodgerDown()
+    }
+    else if (e.key === "ArrowUp") {
+        moveDodgerUp()
+    }
+
+})
+
+function moveDodgerLeft() {
+    let pixNumH = parseInt(dodger.style.left)
+    if (pixNumH > 0) {
+        dodger.style.left = `${pixNumH - 1}px`
+    }
+}
+
+function moveDodgerRight() {
+    let pixNumH = parseInt(dodger.style.left)
+    if (pixNumH < 360) {
+        dodger.style.left = `${pixNumH + 1}px`
+    }
+}
+
+function moveDodgerUp() {
+    let pixNumV = parseInt(dodger.style.bottom)
+    if (pixNumV < 380) {
+        dodger.style.bottom = `${pixNumV + 1}px`
+    }
+}
+
+function moveDodgerDown() {
+    let pixNumV = parseInt(dodger.style.bottom)
+    if (pixNumV > 0) {
+        dodger.style.bottom = `${pixNumV - 1}px`
+    }
+}
